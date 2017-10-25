42af6c648cb20ee83d6b09ee784776d83f119c70
Daine Gostas <daine.gostas@gmail.com>
2017-10-25 12:42:54 -0600

    Not sure what this is

---
+++ HW-4/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(800, 600);
+  background('blue');
+
+  fill('pink');
+  ellipse(300, 200, 100);
+  fill('white');
+  ellipse(275, 175, 40);
+  ellipse(325, 175, 40);
+
+  fill('black');
+  arc(300, 220, 75, 50, 0, PI);
+}
