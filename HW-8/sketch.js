42cec01c2fce1803c46e0cd2732847ecd3788402
Daine Gostas <daine.gostas@gmail.com>
2017-11-05 15:45:31 -0700

    Done with sketch

--- HW-8/sketch.js
+++ HW-8/sketch.js
@@ -1,32 +1,44 @@
+var offset = 0;
 function setup() {
-    createCanvas( windowWidth, 300 );
+    createCanvas( windowWidth, windowHeight );
 }
 
-
-// using 'let' instead of 'var'
-let pos_x = 0;
-let pos_y = 0;
-let d = 30;
-let r = d/2;
-
 function draw() {
-    background( 'rgb(3, 78, 115)' );
 
-    // define drawing parameters
-    fill(255);
-    noStroke();
+  background(0);
+  strokeWeight(4);
+  stroke(random(15));
 
-    // USE A FOR LOOP
+  var y = 0;
+  var x = 0;
 
-    for( var i=0; i < 10; i++ ) {
-        // find the x_pos
-        pos_x = width * i/10 + r;
-        // draw an ellipse
-        ellipse( pos_x, pos_y, d );
+  while (x <= width)  {
+    fill(0, random(255), random(255));
+    ellipse(x, mouseY * 1.5, 25, 25);
+    x = x + 50;
+  }
+
+  for (var x = 0; x <= width; x += 50) {
+    fill(random(255), 0, random(255));
+    ellipse(mouseX * 1.5, x, 25, 25);
+  }
+
+  for (var x = 0; x <= width; x += 50)  {
+    fill(random(255), random(255), 0);
+    ellipse(mouseY * 2, x, 25, 25);
+  }
+
+  for (var x = 0; x <= width; x += 50) {
+    for (var y = 0; y <= height; y += 50) {
+    fill(random(50), random(50), random(50));
+    ellipse(x + offset, y + offset, 25, 25);
+  }
+  }
+  offset = offset + 1;
+  if (offset > 50) {
+        offset = 0;
     }
-
-    pos_y++;
-    if (pos_y > height) {
-        pos_y = 0;
-    }
+    noCursor();
+  fill(random(255), random(255), random(255));
+  ellipse(mouseX, mouseY, 25, 25);
 }
